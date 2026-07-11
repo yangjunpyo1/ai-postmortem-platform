77f9c65fc57bd7cf0af2fbcea6671c327cfbaf63
yangjunpyo1 <wnsvy0128@gmail.com>
2026-07-11 16:28:55 +0900

    fix: backend.tf bucket 이름 복원

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    bucket         = "yp-terraform-state-381492097392"
     key            = "ai-postmortem-platform/terraform.tfstate"
     region         = "ap-northeast-2"
     dynamodb_table = "yp-terraform-lock"
